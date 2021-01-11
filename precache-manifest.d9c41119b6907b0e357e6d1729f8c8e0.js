938efe42254080301e26e1c03a7c7e09f74c9a04
sirmerimre <sirmer.imre@gmail.com>
2021-01-11 17:51:18 +0000

    Updates

---
+++ precache-manifest.d9c41119b6907b0e357e6d1729f8c8e0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c469e2dbc97972b32c2aff94baabd558",
+    "url": "/landing-page-templates.git/index.html"
+  },
+  {
+    "revision": "50f3ff3c41713c31cc87",
+    "url": "/landing-page-templates.git/static/css/2.654353b2.chunk.css"
+  },
+  {
+    "revision": "61d1b14a8fc9d4b7ab5d",
+    "url": "/landing-page-templates.git/static/css/main.00af93bb.chunk.css"
+  },
+  {
+    "revision": "50f3ff3c41713c31cc87",
+    "url": "/landing-page-templates.git/static/js/2.3435e87f.chunk.js"
+  },
+  {
+    "revision": "61d1b14a8fc9d4b7ab5d",
+    "url": "/landing-page-templates.git/static/js/main.d7f34ebb.chunk.js"
+  },
+  {
+    "revision": "526032e80845391c62e7",
+    "url": "/landing-page-templates.git/static/js/runtime~main.b14ea212.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/landing-page-templates.git/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/landing-page-templates.git/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/landing-page-templates.git/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/landing-page-templates.git/static/media/slick.f97e3bbf.svg"
+  }
+]);
