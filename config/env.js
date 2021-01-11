b0837f24bc1266a5b4db15eb9fdc68a244f4e6cf
sirmerimre <sirmer.imre@gmail.com>
2021-01-11 18:48:16 +0000

    update dependencies

--- config/env.js
+++ config/env.js
@@ -61,6 +61,7 @@ process.env.NODE_PATH = (process.env.NODE_PATH || '')
 const REACT_APP = /^REACT_APP_/i;
 
 function getClientEnvironment(publicUrl) {
+  console.log(publicUrl);
   const raw = Object.keys(process.env)
     .filter(key => REACT_APP.test(key))
     .reduce(
