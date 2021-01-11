ed6d356a03a3b797e2f94bcc02a0780f9cd9e211
sirmerimre <sirmer.imre@gmail.com>
2021-01-11 19:12:04 +0000

    Updates

---
+++ precache-manifest.f6a190135aa599bb774fdb2fe091e6cb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d8d9e5f12822bdf8ecf56eee5a5c0c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8781460e3da9d90bde42",
+    "url": "./static/css/2.e38681f0.chunk.css"
+  },
+  {
+    "revision": "b70553e51f7b966124f7",
+    "url": "./static/css/main.3474e328.chunk.css"
+  },
+  {
+    "revision": "8781460e3da9d90bde42",
+    "url": "./static/js/2.69898e7c.chunk.js"
+  },
+  {
+    "revision": "b70553e51f7b966124f7",
+    "url": "./static/js/main.c92af43b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  }
+]);
